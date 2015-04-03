0d34ecb2ca19826c1721cafc8ef6a582211f94f5
JamesHayes <jim@jimandjaime.net>
2015-04-03 15:48:53 -0400

    updated to output PDF images for borough, location, map

---
+++ bin/ratSightings.R
@@ -0,0 +1,54 @@
+# Code for rat sightings visualizations of NYC
+# ~Jim Hayes
+#
+# 04/03/2015
+
+library(ggplot2)
+setwd("~/Documents/dataScience/rat/")
+
+
+# Download and Read census files
+# census
+if(!file.exists("data/census2010.csv")) {
+    fileURL<-"http://www2.census.gov/geo/docs/maps-data/data/rel/zcta_tract_rel_10.txt"
+    download.file(fileURL, destfile="census2010.csv", method="curl")
+}
+censusData <- read.csv("data/census2010.csv", header=TRUE)
+
+
+# Download and read rat data
+if(!file.exists("data/Rat_Sightings.csv")) {
+    #Note, this file was download from NYC Open Data. I couldn't find a directed downlaod to the CSV of rat data, so download it and put it in the appropriate folder.
+    # URL = https://data.cityofnewyork.us/Social-Services/Rat-Sightings/3q43-55fe
+    kill(1)
+}
+
+ratData <- read.csv("data/Rat_Sightings.csv", header=TRUE)
+# Remove entries w/out a reported borough
+unspecifiedBorough <- ratData$Borough=="Unspecified"
+ratData <- ratData[!unspecifiedBorough,]
+
+
+# Breakdown of rat sightings by borough
+b <- ggplot(ratData, aes(factor(Borough)))
+pdf("results/ratsXborough.pdf")
+print(b + geom_bar(width=0.75) + xlab("Borough") + ylab("Rat Sightings") + ggtitle("Rat Sightings by Borough"))
+dev.off()
+
+# Boroguh and Location Type
+l <- ggplot(ratData, aes(Location.Type, fill=Location.Type))
+pdf("results/ratsXlocationType.pdf")
+print(l + geom_bar(width=0.75) + xlab("Borough") + ylab("Rat Sightings") + ggtitle("Rat Sightings by Borough and Structure") + theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position="none"))
+dev.off()
+
+# Type
+#t <- ggplot(ratData, aes(x=factor()))
+
+# Map of rat sightings.  Rats are found everywhere.
+m <- ggplot(ratData, aes(y=Latitude, x=-(Longitude*-1)))
+pdf("results/ratsMap.pdf")
+print(m + geom_point(aes(color=Borough), size=1, alpha=1/2) + xlab("Borough") + ylab("Rat Sightings") + ggtitle("Rat happen everywhere in NYC"))
+dev.off()
+
+#How many sightings have been taken care of?
+table(!is.na(as.Date(ratData$Closed.Date, "%m/%d/%Y")))
